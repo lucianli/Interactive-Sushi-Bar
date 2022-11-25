d0dbf1d7314ef1519d7ce067aedb61074a5dd9b1
Austin Wang <austinxwang@gmail.com>
2022-11-25 15:17:15 -0800

    Added mouse picking for moving trays to table, also added button to eat sushi

--- sushibar.js
+++ sushibar.js
@@ -67,6 +67,25 @@ export class SushiBar extends Scene {
         this.sendMore = false;
         this.trayStartTimes = [];
         this.sushi_rolls = [];
+
+        this.mouse_coord = Mat4.translation(0, 0,0);
+
+        //select sushi is index in sushi_rolls of currently selected sushi
+        this.selected_sushi = null;
+    }
+
+    //Removes a sushi to "eat" it 
+    eat_sushi() {
+        for (let i = 0; i < this.sushi_rolls.length; i++) {
+            let cur_sushi = this.sushi_rolls[i];
+            if (cur_sushi.tray_location == "placed") {
+                if(this.sushi_rolls.length == 1) {
+                    this.sushi_rolls = []
+                } else {
+                    this.sushi_rolls.splice(i, i);
+                }
+            }
+        }
     }
 
     make_control_panel() {
@@ -78,8 +97,31 @@ export class SushiBar extends Scene {
             this.key_triggered_button("View plate", ["Control", "p"], () => this.attached = () => this.plate);
             this.new_line();
             this.key_triggered_button("View sushi", ["Control", "s"], () => this.attached = () => this.sushi);
+            this.new_line();
+            this.key_triggered_button("Eat Sushi", ["Control", "e"], () => this.eat_sushi());
         ;
         this.new_line();
+    }
+
+    my_mouse_down(e, pos, context, program_state) {
+        let pos_ndc_near = vec4(pos[0], pos[1], -1.0, 1.0);
+        let pos_ndc_far  = vec4(pos[0], pos[1],  1.0, 1.0);
+        let center_ndc_near = vec4(0.0, 0.0, -1.0, 1.0);
+        let P = program_state.projection_transform;
+        let V = program_state.camera_inverse;
+        let pos_world_near = Mat4.inverse(P.times(V)).times(pos_ndc_near);
+        let pos_world_far  = Mat4.inverse(P.times(V)).times(pos_ndc_far);
+        let center_world_near  = Mat4.inverse(P.times(V)).times(center_ndc_near);
+        pos_world_near.scale_by(1 / pos_world_near[3]);
+        pos_world_far.scale_by(1 / pos_world_far[3]);
+        center_world_near.scale_by(1 / center_world_near[3]);
+        
+
+        //our scene is somewhere between the near center world and the far world
+        //we use the ratio to find where our table is
+        let near_far_ratio = 0.0105;
+        let world_coord = center_world_near.times(1-near_far_ratio).plus(pos_world_far.times(near_far_ratio));
+        return world_coord;
     }
 
     //Takes time t, an offset for the location (for loops) and a bounds
@@ -97,17 +139,102 @@ export class SushiBar extends Scene {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
             program_state.set_camera(this.initial_camera_location);
+
+
+        let canvas = context.canvas;
+        const mouse_position = (e, rect = canvas.getBoundingClientRect()) =>
+            vec((e.clientX - (rect.left + rect.right) / 2) / ((rect.right - rect.left) / 2),
+                (e.clientY - (rect.bottom + rect.top) / 2) / ((rect.top - rect.bottom) / 2));
+
+        canvas.addEventListener("mousedown", e => {
+            e.preventDefault();
+            const rect = canvas.getBoundingClientRect()
+            let coords = this.my_mouse_down(e, mouse_position(e), context, program_state);
+                
+            
+            //already have a selected sushi that we are placing
+            if (this.selected_sushi != null) {
+
+                //check to see if intersects with any placed sushis
+                for (let i = 0; i < this.sushi_rolls.length; i++) {
+                    let cur_sushi = this.sushi_rolls[i];
+                    let width = 5;
+                    if (cur_sushi.tray_location == "placed" && coords[0] < cur_sushi.placed_coords[0] + width && coords[0] > cur_sushi.placed_coords[0] - width) {
+                        return;
+                    }
+                }
+
+                //Succesfully place sushi and set coordinates
+                coords[1] = 0.0;
+                coords[2] = 12.0;
+                this.sushi_rolls[this.selected_sushi].place_sushi(coords); 
+                this.selected_sushi = null;
+            } else {
+                //don't have a selected sushi
+                if(this.sushi_rolls.length > 0) {
+                    if(coords[1] <4.1 && coords[1] > 2.9) {
+                        for (let i = 0; i < this.sushi_rolls.length; i++) {
+                            let cur_sushi = this.sushi_rolls[i];
+                            //if sushi is not on belt, don't care
+                            if (cur_sushi.tray_location != "on belt") {
+                                continue;
+                            }
+                            t = program_state.animation_time / 1000
+                            let x_dist = 2 * cur_sushi.get_sushi_dist((t-cur_sushi.start_time) / 5.0, 0, 5);
+                            let range = 2;
+                            //check to see if we hit the hitbox of the sushi
+                            if(coords[0] < x_dist + range && coords[0] > x_dist - range) {
+                                //We are now placing that sushi
+                                cur_sushi.tray_location = "placing";
+                                this.selected_sushi = i;
+                            }
+                        }
+                    }
+                }
+            }
+        });
+       
+
+        canvas.addEventListener("mousemove", e => {
+            e.preventDefault();
+            const rect = canvas.getBoundingClientRect()
+            let coords = this.my_mouse_down(e, mouse_position(e), context, program_state);
+            coords[1] = 0.0;
+            coords[2] = 12.0;
+
+            //sets class var mouse coords to the position of the mouse
+            this.mouse_coord = coords;
+        });
         }
 
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, .1, 1000);
 
+        //camera matrix
+        if (this.attached != undefined)
+        {
+            let desired;
+            if (this.attached() == this.initial_camera_location)
+            {
+                desired = this.attached();
+            }
+            else
+            {
+                desired = Mat4.inverse(this.attached());
+            }
+            program_state.camera_inverse = desired.map((x, i) =>
+                Vector.from(program_state.camera_inverse[i]).mix(x, 0.1));
+        }
         const light_position = vec4(0, 15, 0, 1);
         // The parameters of the Light are: position, color, size
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 10000)];
 
-        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        let t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        
+        
+        
         let model_transform = Mat4.identity();
+
 
         //table
         let table_transform = model_transform.times(Mat4.translation(0, -5, 0))
@@ -186,9 +313,46 @@ export class SushiBar extends Scene {
         for (let i = 0; i < this.sushi_rolls.length; i++) {
             let cur_sushi = this.sushi_rolls[i];
 
+            
             let [tray_transform, tray_leg1_transform, tray_leg2_transform] = cur_sushi.get_tray_transforms(t);
             let [roll_transform, first_piece_transform, roll_pieces_transform] = cur_sushi.get_sushi_transforms(t);
             
+            //If we are currently trying to place tray or it has already been placed, 
+            //then we want to alter the sushi location
+            if(cur_sushi.tray_location != "on belt") {
+                let cur_coord = null;
+
+                if (cur_sushi.tray_location == "placing") {
+                    cur_coord = this.mouse_coord; 
+
+                } else if (cur_sushi.tray_location == "placed") {
+                    cur_coord = cur_sushi.placed_coords;
+
+                }
+                
+                let translate_factor = 0.40;
+                tray_transform = Mat4.identity()
+                    .times(Mat4.rotation(Math.PI/2.0 ,0,1,0))
+                    .times(Mat4.scale(1,1,5))
+                    .times(Mat4.translation(translate_factor*cur_coord[2],translate_factor*cur_coord[1], translate_factor*cur_coord[0]))
+                    .times(Mat4.scale(2,0.2,1))
+                    .times(Mat4.translation(0,-17,0));
+
+                tray_leg1_transform = tray_transform.times(Mat4.scale(1,2.5,0.20))
+                    .times(Mat4.translation(0,-1.2,-3));
+
+                tray_leg2_transform = tray_leg1_transform.times(Mat4.translation(0,0,6));
+
+                roll_transform = Mat4.translation(translate_factor*cur_coord[0]*5,translate_factor*cur_coord[1]-2.25, translate_factor*cur_coord[2]-9.75)
+                    .times(Mat4.rotation(Math.PI/8, 0, 1, 0));
+                first_piece_transform = roll_transform.times(Mat4.translation(-1.5, -0.70, 0))
+                    .times(Mat4.rotation(Math.PI/2.0, 1, 0, 0))
+                    .times(Mat4.scale(1, 1, 1/1.5));
+
+                roll_pieces_transform = roll_transform.times(Mat4.rotation(Math.PI / 2.0, 0, 1, 0))
+                    .times(Mat4.scale(1, 1, 1/1.5));
+            }
+
             //draw horizontal sushi
             this.shapes.capped_cylinder.draw(context, program_state, first_piece_transform, this.materials.phong_white.override({color: hex_color("#107528")}));
             
@@ -208,21 +372,7 @@ export class SushiBar extends Scene {
             this.shapes.cube.draw(context, program_state, tray_leg2_transform, this.materials.phong_white);
         }
 
-        //camera matrix
-        if (this.attached != undefined)
-        {
-            let desired;
-            if (this.attached() == this.initial_camera_location)
-            {
-                desired = this.attached();
-            }
-            else
-            {
-                desired = Mat4.inverse(this.attached());
-            }
-            program_state.camera_inverse = desired.map((x, i) =>
-                Vector.from(program_state.camera_inverse[i]).mix(x, 0.1));
-        }
+        
     }
 }
 
